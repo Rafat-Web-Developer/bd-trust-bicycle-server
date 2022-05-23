e6d31de5080992a43157e839ef02c3bac779c130
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-24 05:59:18 +0600

    use cors and dotenv

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("BD Trust Bicycle Server Working... Alhamdulillah!!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`BD Trust Bicycle app listening on port ${port}`);
 });
