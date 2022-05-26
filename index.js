410b76e8f524679da0e593807a8d68f93559d3be
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-26 08:32:55 +0600

    Create an api for delete order

--- index.js
+++ index.js
@@ -232,6 +232,13 @@ async function run() {
       res.send({ success: true, result });
     });
 
+    app.delete("/order/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // All Orders API End
 
     // ---->All API End<-----
