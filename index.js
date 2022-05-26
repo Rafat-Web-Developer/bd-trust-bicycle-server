5ab13d6385b73b45406ba990bbe8dd12edf81980
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-05-26 09:51:42 +0600

    Create an api for load single order

--- index.js
+++ index.js
@@ -229,6 +229,13 @@ async function run() {
       res.send(orders);
     });
 
+    app.get("/order/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await ordersCollection.findOne(query);
+      res.send(order);
+    });
+
     app.post("/order", verifyJWT, async (req, res) => {
       const order = req.body;
       const query = {
